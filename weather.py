d5fc3c159eea30bceef598e990b89451b4dc21ec
AnastasiaKedrina <113825953+AnastasiaKedrina@users.noreply.github.com>
2024-05-25 14:36:27 +0800

    +serial чтобы передавать погоду

--- weather.py
+++ weather.py
@@ -1,6 +1,11 @@
 appid = '9d763e7cd096892b5a50b5df25faf8ed'# полученный при регистрации на OpenWeatherMap.org. Что-то вроде такого набора букв и цифр: '6d8e495ca73d5bbc1d6bf8ebd52c4123'
 
 import requests
+import serial
+import time
+
+arduino = serial.Serial(port='COM9', baudrate=9600, timeout=.1)
+
 
 # Проверка наличия в базе информации о нужном населенном пункте
 def get_city_id(s_city_name):
@@ -10,9 +15,9 @@ def get_city_id(s_city_name):
         data = res.json()
         cities = ['{} ({})'.format(d['name'], d['sys']['country'])
                   for d in data['list']]
-        print('city:', cities)
+        # print('city:', cities)
         city_id = data['list'][0]['id']
-        print('city_id=', city_id)
+        # print('city_id=', city_id)
     except Exception as e:
         print('Exception (find):', e)
         pass
@@ -22,7 +27,8 @@ def get_city_id(s_city_name):
 
 # Запрос текущей погоды
 def request_current_weather(city_id):
-    weather_descriptions_array = ['Thunderstorm', 'Drizzle', 'Rain', 'Snow', 'Atmosphere', 'Clear', 'Clouds']
+    weather_descriptions_array = ['Clear', 'Clouds', 'Rain'] # номер погоды от 0 до 2, 3 варианта
+    weather_descriptions_array = ['Clear', 'Clouds', 'Rain', 'Snow', 'Atmosphere', 'Drizzle', 'Thunderstorm'] # номер погоды от 0 до 7 (8 вариантов)
     try:
         res = requests.get('http://api.openweathermap.org/data/2.5/weather',
                      params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
@@ -31,16 +37,32 @@ def request_current_weather(city_id):
         weather = data['weather'][0]['main'] #общее описание погоды
         weather_num = weather_descriptions_array.index(weather)
         temperature = data['main']['temp']
-        print('conditions:', weather)
-        print('номер в массиве:', weather_num)
-        print('temp:', temperature)
+        # print('conditions:', weather)
+        # print('номер в массиве:', weather_num)
+        # print('temp:', temperature)
         # print('data:', data)
+        return [temperature, weather_num]
     except Exception as e:
         print('Exception (weather):', e)
         pass
 
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
 
-city = input('Введите город: ')
+
+
+# city = input('Введите город: ')
+city = 'Иркутск'
+# city = 'Москва'
 city_id = get_city_id(city)
+temperature, weather_num = request_current_weather(city_id)
 
-request_current_weather(city_id)
+separator = '-'
+# temperature = int(temperature)
+weather_string = f'{temperature}{separator}{weather_num}'
+value_from_arduino = write_read(str(weather_num))
+print(weather_num)
+print(value_from_arduino)
