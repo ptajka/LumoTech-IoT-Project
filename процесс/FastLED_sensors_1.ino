03df60c4da6d9fe6683a76cacf0d30a068c7a4b8
Nastya Kedrina <kedrina1111@gmail.com>
2024-06-07 17:40:44 +0800

    Создала папки

---
+++ процесс/FastLED_sensors_1.ino
@@ -0,0 +1,355 @@
+/// @file    Noise.ino
+/// @brief   Demonstrates how to use noise generation on a 2D LED matrix
+/// @example Noise.ino
+
+#include <FastLED.h>
+
+#define NUM_LEDS 60
+#define FRAMES_PER_SECOND  120
+CRGB leds[NUM_LEDS];
+
+static uint16_t x;
+static uint16_t y;
+static uint16_t z;
+
+uint16_t speed = 2;  // 1-100
+uint16_t scale = 1;  // 1-4000
+uint16_t noise[60];
+
+uint16_t BRIGHTNESS = 10;
+
+//---------------------энкодер
+#define CLK 2
+#define DT 3
+#define SW 4
+
+int counter = 50;
+int encoder_brightness_step = 20;
+int currentStateCLK;
+int lastStateCLK;
+String currentDir = "";
+unsigned long lastButtonPress = 0;
+
+
+//---------------------датчик движения
+#define OUT 5
+
+int current_val_pir = 0;
+int state_pir = LOW;
+
+//---------------------
+
+String serialString = "";
+int delim = 0;
+int weather_num = 0;
+int temperature = 0;
+int move_trig, LM_state, weather_val, encoder_val = 0;
+
+uint8_t ihue = 0;
+uint8_t hue_offset = 0;
+uint8_t hue = 0;
+uint8_t hue_saturation = 0;
+uint8_t hue_value = 0;
+uint8_t color_scale = 1;
+
+uint8_t gHue = 0;
+
+void setup() {
+  Serial.begin(9600);
+  delay(3000);
+  FastLED.addLeds<WS2812, 13, RGB>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+
+  x = random16();
+  y = random16();
+  z = random16();
+
+  pinMode(CLK, INPUT);
+  pinMode(DT, INPUT);
+  pinMode(SW, INPUT_PULLUP);
+  lastStateCLK = digitalRead(CLK);
+}
+
+void fillnoise8() {
+  for (int i = 0; i < 60; i++) {
+    int ioffset = scale * i;
+    noise[i] = inoise8(x + ioffset, 0, z);
+  }
+  z += speed;
+
+  
+  for (int i = 0; i < 60; i++) {
+    hue = ihue + (noise[i] >> color_scale) + hue_offset;
+    hue_value = noise[i];
+
+    if (LM_state == 0) {
+      if ((temperature < 3) && (temperature > -3)) {
+        hue_saturation = 50;
+      } else if ((temperature < 6) && (temperature > -6)) {
+        hue_saturation = 100;
+      }
+      if ((temperature < 10) && (temperature > -10)) {
+        hue_saturation = 150;
+      } else {
+        hue_saturation = 255 - (noise[i] % 50);
+      }
+    } 
+    leds[i] = CHSV(hue, hue_saturation, hue_value);
+  }
+}
+
+
+void loop() {
+  FastLED.delay(1000/FRAMES_PER_SECOND); 
+
+  get_encoder_value();
+  set_brightness_by_encoder();
+
+  if (Serial.available() > 0) {
+    sensors_and_weather_by_serial();
+  }
+
+  set_LM_state();
+  set_pir_mode();
+  FastLED.show();
+}
+
+
+void set_LM_state() {
+  EVERY_N_MILLISECONDS( 20 ) { gHue++; }
+  if(LM_state > 1){
+      hue_saturation = 255;
+  }
+  switch (LM_state) {
+    case 8:
+      juggle();
+      break;
+    case 7:
+    // голубой
+      fillnoise8();
+      temperature = -1;
+      weather_num = 1;
+      set_weather_num();
+      set_color_by_temperature();
+      break;
+    case 6:
+      sinelon();
+      break;
+    case 5:
+    // красный
+      fillnoise8();
+      temperature = 30;
+      weather_num = 2;
+      set_weather_num();
+      set_color_by_temperature();
+      break;
+    case 4:
+      confetti();
+      break;
+    case 3:
+    // синий
+      fillnoise8();
+      temperature = -30;
+      weather_num = 1;
+      set_weather_num();
+      set_color_by_temperature();
+      break;
+    case 2:
+      rainbow();
+      break;
+    case 1:
+    // белый
+      ihue = 0;
+      hue_saturation = 0;
+      fillnoise8();
+      break;
+    case 0:
+    // цвета зависят от погоды
+      fillnoise8();
+      set_weather_num();
+      set_color_by_temperature();
+      break;
+    default:
+      fillnoise8();
+      set_weather_num();
+      set_color_by_temperature();
+      break;
+  }
+}
+
+void set_weather_num() {
+  switch (weather_num) {
+    case 2:
+      // Rain, Snow
+      speed = 4;
+      scale = 150;
+      color_scale = 5;
+      hue_offset = 0;
+      break;
+
+    case 1:
+      // Clouds
+      speed = 3;
+      scale = 50;
+      color_scale = 2;
+      hue_offset = -20;
+      break;
+
+    case 0:
+      // Clear
+      speed = 1;
+      scale = 10;
+      color_scale = 1;
+      hue_offset = -50;
+      break;
+
+    default:
+      speed = 4;
+      scale = 150;
+      color_scale = 5;
+      hue_offset = 0;
+      break;
+  }
+}
+
+void set_color_by_temperature() {
+  // 0 желтый
+  // 40 красный
+  // 50 розовый
+  // 60 фиолетовый
+  // 110 синий
+  // 130 голубой
+  // 220 зеленый
+  if (temperature >= 0) {
+    // для теплой погоды - теплые цвета
+    ihue = temperature + 60;
+  } else {
+    // для холодной погоды - холодные цвета
+    ihue = temperature * 2 + 230;
+  }
+}
+
+void sensors_and_weather_by_serial() {
+  // на вход строка из LM_state, temperature, weather_num
+  char separator = ',';
+  serialString = Serial.readString();
+  delim = 0;
+
+  delim = serialString.indexOf(',');
+  LM_state = serialString.substring(0, delim).toInt();
+  serialString = serialString.substring(delim + 1, serialString.length());
+
+  delim = serialString.indexOf(separator);
+  temperature = serialString.substring(0, delim).toFloat();
+  serialString = serialString.substring(delim + 1, serialString.length());
+
+  delim = serialString.indexOf(separator);
+  weather_num = serialString.substring(0, delim).toInt();
+  serialString = serialString.substring(delim + 1, serialString.length());
+
+  // Serial.print(LM_state);
+  // Serial.print(separator);
+  // Serial.print(temperature);
+  // Serial.print(separator);
+  // Serial.println(weather_num);
+}
+
+void get_encoder_value() {
+  currentStateCLK = digitalRead(CLK);
+  if (currentStateCLK != lastStateCLK && currentStateCLK == 1) {
+    if (digitalRead(DT) != currentStateCLK) {
+      counter = counter - encoder_brightness_step;
+    } else {
+      counter = counter + encoder_brightness_step;
+    }
+    // Serial.print("Counter: ");
+    // Serial.println(counter);
+  }
+  lastStateCLK = currentStateCLK;
+  int btnState = digitalRead(SW);
+
+  if (btnState == LOW) {
+    if (millis() - lastButtonPress > 50) {
+      // Serial.println("Button pressed!");
+      LM_state += 1;
+      if (LM_state>8){ LM_state = 0;}
+    }
+    lastButtonPress = millis();
+  }
+}
+
+void set_brightness_by_encoder() {
+  if (counter > 255) {
+    counter = 255;
+  } else if (counter < 0) {
+    counter = 0;
+  }
+  BRIGHTNESS = counter;
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
+
+void set_pir_mode() {
+  current_val_pir = digitalRead(OUT);
+  if (current_val_pir == HIGH) {            
+    counter = 255;
+    if (state_pir == LOW) {
+      Serial.println("Smth moving");
+      state_pir = HIGH;
+    }
+  } 
+  else {
+    counter = 0;
+    if (state_pir == HIGH) {
+      Serial.println("No moving");
+      state_pir = LOW;
+    }
+  }
+  BRIGHTNESS = counter;
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
+
+
+
+void rainbow() 
+{
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+void rainbowWithGlitter() 
+{
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+
+void confetti() 
+{
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon()
+{
+  fadeToBlackBy( leds, NUM_LEDS, 1);
+  int pos = beatsin16( 13, 0, NUM_LEDS-1);
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+
+void juggle() {
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  uint8_t dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
+    dothue += 20;
+  }
+}
---
+++ процесс/weather_mqtt_bot/weather_mqtt_bot copy.py
@@ -0,0 +1,192 @@
+appid = '9d763e7cd096892b5a50b5df25faf8ed'  # полученный при регистрации на OpenWeatherMap.org. Что-то вроде такого набора букв и цифр: '6d8e495ca73d5bbc1d6bf8ebd52c4123'
+
+import requests
+import serial, serial.tools.list_ports
+import time, random
+import paho.mqtt.client as mqtt_client
+import paho.mqtt.subscribe as subscribe
+
+# from bot import run
+
+
+# def find_COM_port():
+#     try:
+#         # Find and open the COM port
+#         ports = serial.tools.list_ports.comports()
+#         port = next((p.device for p in ports), None)
+#         if port is None:
+#             raise ValueError("No COM port found.")
+#         arduino = serial.Serial(port, baudrate=9600)
+#     except ValueError as ve:
+#         print("Error:", str(ve))
+#     except serial.SerialException as se:
+#         print("Serial port error:", str(se))
+#     except Exception as e:
+#         print("An error occurred:", str(e))
+#     return arduino
+
+
+# -----------------------------------------------------------
+def get_city_id(s_city_name):
+    # Проверка наличия в базе информации о нужном населенном пункте
+    try:
+        res = requests.get('http://api.openweathermap.org/data/2.5/find',
+                           params={'q': s_city_name, 'type': 'like', 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+        data = res.json()
+        cities = ['{} ({})'.format(d['name'], d['sys']['country'])
+                  for d in data['list']]
+        # print('city:', cities)
+        city_id = data['list'][0]['id']
+        # print('city_id=', city_id)
+    except Exception as e:
+        print('Exception (find):', e)
+        pass
+    assert isinstance(city_id, int)
+    return city_id
+
+
+def request_current_weather(city_id):
+    # Запрос текущей погоды
+    weather_descriptions_array = ['Clear', 'Clouds', 'Rain']  # номер погоды от 0 до 2, три варианта
+    weather_descriptions_array = ['Clear', 'Clouds', 'Rain', 'Snow', 'Atmosphere', 'Drizzle',
+                                  'Thunderstorm']  # все варианты, 3-7 отмечаются как дождь (2)
+    try:
+        res = requests.get('http://api.openweathermap.org/data/2.5/weather',
+                           params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+        data = res.json()
+        # weather = data['weather'][0]['description'] # точное описание погоды
+        weather = data['weather'][0]['main']  # общее описание погоды
+        weather_num = weather_descriptions_array.index(weather)
+        if (weather_num > 2):
+            weather_num = 2
+        temperature = data['main']['temp']
+        # print('conditions:', weather)
+        # print('номер в массиве:', weather_num)
+        # print('temp:', temperature)
+        # print('data:', data)
+        return [temperature, weather_num]
+    except Exception as e:
+        print('Exception (weather):', e)
+        pass
+
+
+def get_weather(city):
+    # city = 'Иркутск'
+    # city = 'Москва'
+    city_id = get_city_id(city)
+    temperature, weather_num = request_current_weather(city_id)
+
+    separator = ','
+    # temperature = int(temperature)
+    weather_string = f'{temperature}{separator}{weather_num}'
+    return [temperature, weather_num]
+    # weather_data = weather_num
+    # return weather_data
+
+
+# -----------------------------------------------------------
+def send_MQTT(data):
+    value_from_arduino = serial_write(str(data))
+    print(f'MQTT data = {data}')
+    # print(f'MQTT data from arduino = {value_from_arduino}')
+
+
+def on_message(client, userdata, message):
+    global LM_state
+    LM_state = str(message.payload.decode("utf-8"))
+    print(LM_state)
+    # send_MQTT(LM_state)
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+    else:
+        print("Failed to connect, return code %d\n", rc)
+
+
+def mqtt_connection(topic):
+    client = mqtt_client.Client(f'client_{random.randint(10000, 99999)}')
+    client.on_message = on_message
+    client.on_connect = on_connect
+
+    client.connect(broker)
+    client.loop_start()
+    client.subscribe(topic)
+    # client.publish(topic, value)
+    time.sleep(50)
+    client.disconnect()
+    client.loop_stop()
+
+
+# -----------------------------------------------------------
+def send_weather():
+    weather_str = get_weather()
+    value_from_arduino = serial_write(str(weather_str))
+    print(f'weather = {weather_str}')
+    # print(f'weather from arduino = {value_from_arduino}')
+
+
+def send_mqtt_and_weather(LM_states, LM_state, city):
+    separator = ','
+    print(f'Текущий режим: ({LM_state}){LM_states[LM_state]}')
+
+    if LM_state == 0:
+        temperature, weather_num = get_weather(city)
+        serial_string = f'{LM_state}{separator}{temperature}{separator}{weather_num}'
+    else:
+        serial_string = f'{LM_state}{separator}0{separator}0'
+    value_from_arduino = serial_write(serial_string)
+    print(f'По serial отправлена строка {serial_string}')
+    # print(f'string from arduino = {value_from_arduino}')
+
+
+# -----------------------------------------------------------
+def serial_write(data):
+    arduino.write(bytes(data, 'utf-8'))
+
+
+def serial_read():
+    data = arduino.readline()
+    return data
+
+
+# -----------------------------------------------------------
+
+def find_COM_port():
+    # подключение к порту Arduino
+    for COM_port in serial.tools.list_ports.comports():
+        # print(COM_port.device)
+        COM_port = COM_port.device
+        try:
+            arduino = serial.Serial(port=COM_port, baudrate=9600)
+        except:
+            continue
+    return arduino
+
+def set_MQTT():
+    broker = "broker.emqx.io"
+    LM_topic = "leap_motion_states"
+
+
+    client = mqtt_client.Client(f'client_{random.randint(10000, 99999)}')
+    client.on_message = on_message
+    client.on_connect = on_connect
+    client.connect(broker)
+    client.loop_start()
+
+# city = input('Введите город: ')
+city = 'Иркутск'
+arduino = find_COM_port()
+
+# run()
+def send_serial(city):
+    LM_states = ['Погода', 'Белый', 'Радуга', 'Синий', 'Конфетти', 'Красный', 'Синелон', 'Голубой', 'Джагл']
+    LM_state = 0
+    # while True:
+    #    city = get_city()
+    #    print(city)
+    # LM_state = subscribe.simple(LM_topic, hostname=broker)
+    send_mqtt_and_weather(LM_states, int(float(LM_state)), city)
+    # send_mqtt_and_weather(int(float(LM_state.payload)), city)
+
