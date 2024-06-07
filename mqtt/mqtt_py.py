129f92eeabaf8893c932aa4f43e53fbb682e74c2
AnastasiaKedrina <113825953+AnastasiaKedrina@users.noreply.github.com>
2024-06-07 11:41:26 +0800

    Update mqtt_py.py

--- mqtt/mqtt_py.py
+++ mqtt/mqtt_py.py
@@ -1,32 +1,42 @@
-import time
-import paho.mqtt.client as mqtt_client
-import random
-import serial
+# import time
+# import paho.mqtt.client as mqtt_client
+# import random
+# import serial
+
+# broker="broker.emqx.io"
+
+# responses = {'d': 7, 
+#              'u': 6}
+
+# def on_message(client, userdata, message):
+#     data = str(message.payload.decode("utf-8"))
+#     print("received message =", data)
+    
+# def on_connect(client, userdata, flags, rc):
+#     if rc == 0:
+#         print("Connected to MQTT Broker!")
+#     else:
+#         print("Failed to connect, return code %d\n", rc)
+
+
+# client= mqtt_client.Client(f'client_{random.randint(10000, 99999)}') 
+# client.on_message=on_message
+# client.on_connect=on_connect
+
+# client.connect(broker) 
+# client.loop_start() 
+# print("Subcribing")
+# client.subscribe("testData")
+# time.sleep(50)
+# client.disconnect()
+# client.loop_stop()
+
+import paho.mqtt.subscribe as subscribe
 
 broker="broker.emqx.io"
+LM_topic = "leap_motion_states"
+LM_state = subscribe.simple(LM_topic, hostname=broker)
+print(LM_state.payload)
 
-responses = {'d': 7, 
-             'u': 6}
-
-def on_message(client, userdata, message):
-    data = str(message.payload.decode("utf-8"))
-    print("received message =", data)
-    
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected to MQTT Broker!")
-    else:
-        print("Failed to connect, return code %d\n", rc)
-
-
-client= mqtt_client.Client(f'client_{random.randint(10000, 99999)}') 
-client.on_message=on_message
-client.on_connect=on_connect
-
-client.connect(broker) 
-client.loop_start() 
-print("Subcribing")
-client.subscribe("testData")
-time.sleep(50)
-client.disconnect()
-client.loop_stop()
+# import paho.mqtt.publish as publish
+# publish.single("paho/test/single", "payload", hostname="mqtt.eclipse.org")
