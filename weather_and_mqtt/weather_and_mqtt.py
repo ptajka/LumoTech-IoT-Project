6fb9a9655bae145ee3d3eb8ae62ebb2a86a247fe
Yana Vetvitskaya <151332630+ptajka@users.noreply.github.com>
2024-06-07 15:55:37 +0800

    Update weather_and_mqtt.py

--- weather_and_mqtt/weather_and_mqtt.py
+++ weather_and_mqtt/weather_and_mqtt.py
@@ -1,10 +1,13 @@
-appid = '9d763e7cd096892b5a50b5df25faf8ed'# полученный при регистрации на OpenWeatherMap.org. Что-то вроде такого набора букв и цифр: '6d8e495ca73d5bbc1d6bf8ebd52c4123'
+appid = '9d763e7cd096892b5a50b5df25faf8ed'  # полученный при регистрации на OpenWeatherMap.org. Что-то вроде такого набора букв и цифр: '6d8e495ca73d5bbc1d6bf8ebd52c4123'
 
 import requests
 import serial, serial.tools.list_ports
 import time, random
 import paho.mqtt.client as mqtt_client
 import paho.mqtt.subscribe as subscribe
+
+from bot import get_city
+
 
 # def find_COM_port():
 #     try:
@@ -23,14 +26,12 @@ import paho.mqtt.subscribe as subscribe
 #     return arduino
 
 
-
-
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 def get_city_id(s_city_name):
     # Проверка наличия в базе информации о нужном населенном пункте
     try:
         res = requests.get('http://api.openweathermap.org/data/2.5/find',
-                     params={'q': s_city_name, 'type': 'like', 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+                           params={'q': s_city_name, 'type': 'like', 'units': 'metric', 'lang': 'ru', 'APPID': appid})
         data = res.json()
         cities = ['{} ({})'.format(d['name'], d['sys']['country'])
                   for d in data['list']]
@@ -46,16 +47,17 @@ def get_city_id(s_city_name):
 
 def request_current_weather(city_id):
     # Запрос текущей погоды
-    weather_descriptions_array = ['Clear', 'Clouds', 'Rain'] # номер погоды от 0 до 2, три варианта
-    weather_descriptions_array = ['Clear', 'Clouds', 'Rain', 'Snow', 'Atmosphere', 'Drizzle', 'Thunderstorm'] # все варианты, 3-7 отмечаются как дождь (2)
+    weather_descriptions_array = ['Clear', 'Clouds', 'Rain']  # номер погоды от 0 до 2, три варианта
+    weather_descriptions_array = ['Clear', 'Clouds', 'Rain', 'Snow', 'Atmosphere', 'Drizzle',
+                                  'Thunderstorm']  # все варианты, 3-7 отмечаются как дождь (2)
     try:
         res = requests.get('http://api.openweathermap.org/data/2.5/weather',
-                     params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+                           params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
         data = res.json()
         # weather = data['weather'][0]['description'] # точное описание погоды
-        weather = data['weather'][0]['main'] #общее описание погоды
+        weather = data['weather'][0]['main']  # общее описание погоды
         weather_num = weather_descriptions_array.index(weather)
-        if (weather_num>2):
+        if (weather_num > 2):
             weather_num = 2
         temperature = data['main']['temp']
         # print('conditions:', weather)
@@ -66,6 +68,7 @@ def request_current_weather(city_id):
     except Exception as e:
         print('Exception (weather):', e)
         pass
+
 
 def get_weather(city):
     # city = 'Иркутск'
@@ -81,30 +84,33 @@ def get_weather(city):
     # return weather_data
 
 
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 def send_MQTT(data):
     value_from_arduino = serial_write(str(data))
     print(f'MQTT data = {data}')
     # print(f'MQTT data from arduino = {value_from_arduino}')
+
 
 def on_message(client, userdata, message):
     global LM_state
     LM_state = str(message.payload.decode("utf-8"))
     print(LM_state)
     # send_MQTT(LM_state)
-    
+
+
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected to MQTT Broker!")
     else:
         print("Failed to connect, return code %d\n", rc)
 
-def mqtt_connection(topic):
-    client= mqtt_client.Client(f'client_{random.randint(10000, 99999)}') 
-    client.on_message=on_message
-    client.on_connect=on_connect
 
-    client.connect(broker) 
+def mqtt_connection(topic):
+    client = mqtt_client.Client(f'client_{random.randint(10000, 99999)}')
+    client.on_message = on_message
+    client.on_connect = on_connect
+
+    client.connect(broker)
     client.loop_start()
     client.subscribe(topic)
     # client.publish(topic, value)
@@ -113,9 +119,9 @@ def mqtt_connection(topic):
     client.loop_stop()
 
 
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 def send_weather():
-    weather_str = get_weather()   
+    weather_str = get_weather()
     value_from_arduino = serial_write(str(weather_str))
     print(f'weather = {weather_str}')
     # print(f'weather from arduino = {value_from_arduino}')
@@ -125,7 +131,7 @@ def send_mqtt_and_weather(LM_state, city):
     separator = ','
     print(f'Текущий режим: ({LM_state}){LM_states[LM_state]}')
 
-    if LM_state==0:
+    if LM_state == 0:
         temperature, weather_num = get_weather(city)
         serial_string = f'{LM_state}{separator}{temperature}{separator}{weather_num}'
     else:
@@ -135,14 +141,17 @@ def send_mqtt_and_weather(LM_state, city):
     # print(f'string from arduino = {value_from_arduino}')
 
 
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 def serial_write(data):
     arduino.write(bytes(data, 'utf-8'))
-    
+
+
 def serial_read():
     data = arduino.readline()
     return data
-#-----------------------------------------------------------
+
+
+# -----------------------------------------------------------
 
 def find_COM_port():
     # подключение к порту Arduino
@@ -155,32 +164,25 @@ def find_COM_port():
             continue
     return arduino
 
-broker="broker.emqx.io"
-LM_topic = "leap_motion_states"
 
+broker = "broker.emqx.io"
+LM_topic = "leap_motion_states"
 
 LM_state = 0
 LM_states = ['Погода', 'Белый', 'Радуга', 'Синий', 'Конфетти', 'Красный', 'Синелон', 'Голубой', 'Джагл']
 
-
-client= mqtt_client.Client(f'client_{random.randint(10000, 99999)}') 
-client.on_message=on_message
-client.on_connect=on_connect
-client.connect(broker) 
+client = mqtt_client.Client(f'client_{random.randint(10000, 99999)}')
+client.on_message = on_message
+client.on_connect = on_connect
+client.connect(broker)
 client.loop_start()
 
 # city = input('Введите город: ')
-city = 'Иркутск'
+# city = 'Иркутск'
 
 arduino = find_COM_port()
-
-while True:
-    LM_state = subscribe.simple(LM_topic, hostname=broker)
-    send_mqtt_and_weather(int(float(LM_state.payload)), city)
-
-# client.disconnect()
-# client.loop_stop()
-# print(LM_state)
-
-# while True:
-#     time.sleep(0.05)
+city = get_city()
+print(city)
+#while True:
+#    LM_state = subscribe.simple(LM_topic, hostname=broker)
+#    send_mqtt_and_weather(int(float(LM_state.payload)), city)
